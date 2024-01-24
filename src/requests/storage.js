6ecf4cbdc0b46370b695ea9747eb14cf630143e7
mgrebeniuk <Maxim13parkour@gmail.com>
2024-01-24 22:32:34 +0300

    test

--- src/requests/storage.js
+++ src/requests/storage.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
   api(data) {
-    return axios.post('/storage?type=IMAGE', data, {
+    return axios.post('http://79.174.84.200:82/api/v1/storage', data, {
       headers: { 'Content-Type': 'multipart/form-data' },
     });
   },
