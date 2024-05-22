5855c5b61b987cf6a5c4598f1b99cfa87fc442f8
evgeniiijjj <evgenijjj78@gmail.com>
2024-05-22 20:27:00 +0300

    Revert "proxy 8080"
    This reverts commit 2743a7cb14cdbc5d3147870f56bea89572658c2a.

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
-    proxy: 'http://79.174.84.200:8080/',
+    proxy: 'http://79.174.84.200:80/',
     // proxy: 'http://82.202.214.42:8899/',
     //  proxy: 'http://212.22.70.159:1337/',
     // proxy: 'http://localhost:8080/',
