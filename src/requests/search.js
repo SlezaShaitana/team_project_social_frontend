95fb88b297e5c37259722e34c78fc2886f8ae9c3
Dmitriy Utkin <gondrlef@gmail.com>
2024-06-04 21:46:44 +0300

    Update search.js rollback to the initial option

--- src/requests/search.js
+++ src/requests/search.js
@@ -1,25 +1,13 @@
 import axios from 'axios';
 
-function serializeQueryParams(params) {
-  return Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-    .join('&');
-}
-
 export default {
   // Поиск пользователей по запросу
   users(query) {
-    if (typeof query === 'object' && query !== null && !Array.isArray(query)) {
-      const serializedParams = serializeQueryParams(query);
-      return axios.get(`account/search?${serializedParams}&isDeleted=false`);
-    }
-    if (typeof query === 'string') {
-      return axios.get(`account/search?${query}&isDeleted=false`);
-    }
-    if (Array.isArray(query)) {
-      const serializedParams = query.map(serializeQueryParams).join('&');
-      return axios.get(`friends?statusCode=FRIEND&${serializedParams}`);
-    }
+    if (Array.isArray(query)) return axios.get(`account/search?${query.join('&')}&isDeleted=false`);
+    if (typeof query === 'string') return axios.get(`account/search?${query}`);
+    if (Array.isArray(query)) return axios.get(`friends?statusCode=FRIEND&${query.join('&')}`);
+    if (typeof query === 'string') return axios.get(`friends?statusCode=FRIEND&${query}`);
+
     throw new TypeError('Недопустимое значение запроса');
   },
 
