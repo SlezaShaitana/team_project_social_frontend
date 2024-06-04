7e0ae7f5f324752a0211ba1f2190f093e2b6c00d
Dmitriy Utkin <gondrlef@gmail.com>
2024-06-04 21:58:49 +0300

    Update search.js # 5

--- src/requests/search.js
+++ src/requests/search.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
+function serializeQueryParams(params) {
+  return Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .join('&');
+}
+
 export default {
   // Поиск пользователей по запросу
   users(query) {
-    if (Array.isArray(query)) return axios.get(`account/search?${query.join('&')}&isDeleted=false`);
+    if (Array.isArray(query)) return axios.get(`account/search?${serializeQueryParams(query)}&isDeleted=false`);
     if (typeof query === 'string') return axios.get(`account/search?${query}`);
-    if (Array.isArray(query)) return axios.get(`friends?statusCode=FRIEND&${query.join('&')}`);
+    if (Array.isArray(query)) return axios.get(`friends?statusCode=FRIEND&${serializeQueryParams(query)}`);
     if (typeof query === 'string') return axios.get(`friends?statusCode=FRIEND&${query}`);
 
     throw new TypeError('Недопустимое значение запроса');
   },
-
 
   recomendationFrends(query) {
     if (Array.isArray(query)) return axios.get(`account/search?${query.join('&')}&isDeleted=false&showFriends=false`)
